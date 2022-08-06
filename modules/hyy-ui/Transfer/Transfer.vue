fdf404f9a80cde0ac246cea740f29510a93c6d65
3443718488@qq.com <3443718488@qq.com>
2022-08-06 18:37:38 +0800

    样式

--- modules/hyy-ui/Transfer/Transfer.vue
+++ modules/hyy-ui/Transfer/Transfer.vue
@@ -8,9 +8,17 @@
 		<div class="transfer">
 			<div class="box left-list">
 				<h1 class="list-title">{{ leftTitle }}</h1>
+				<div>
+				</div>
 			</div>
-			<div class="box button-group"></div>
-			<div class="box right-list"></div>
+			<div class="box button-group">
+				<button>&lt;</button>
+				<button>&gt;</button>
+			</div>
+			<div class="box right-list">
+				<h1 class="list-title">{{ rightTitle }}</h1>
+				<div></div>
+			</div>
 		</div>
 	</div>
 </template>
@@ -30,6 +38,59 @@ const { leftTitle } = useComputedData(props.data, targetIndex)
 
 </script>
 
-<style lang="" scoped>
-	
+<style lang="scss" scoped>
+.transfer {
+	display: flex;
+	flex-direction: row;
+	width: 360px;
+	height: 300px;
+	border: 1px solid #ddd;
+
+	.box {
+		width: 120px;
+		height: 100%;
+
+		.list-title {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			height: 38px;
+			font-weight: normal;
+			margin: 0;
+			color: #666;
+			border-bottom: 1px solid #ddd;
+			background-color: #efefef;
+			font-size: 14px;
+		}
+
+		&.button-group {
+			display: flex;
+			justify-content: center;
+			align-items: center;
+			border-left: 1px solid #ddd;
+			border-right: 1px solid #ddd;
+
+			button {
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				border: none;
+				outline: none;
+				width: 38px;
+				height: 38px;
+				background-color: orange;
+				color: #fff;
+				border-radius: 5px;
+
+				&:disabled {
+					opacity: .6;
+				}
+
+				&:nth-child(2) {
+					margin-left: 10px;
+				}
+			}
+		}
+	}
+}
 </style>
